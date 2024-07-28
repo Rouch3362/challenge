4abe095816eb10e69647e2442937f413aba6bed5
unknown <rouchashoori@gmail.com>
2024-07-28 15:40:30 +0330

    added brand model

--- brands/models.py
+++ brands/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django_countries.fields import CountryField
 
-# Create your models here.
+class Brand(models.Model):
+    name    = models.CharField(max_length=256)
+    region  = CountryField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+    def __str__(self) -> str:
+        return self.name
