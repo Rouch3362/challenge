b71143044b70e533bf896b94e0df8e99d8e69d9d
unknown <rouchashoori@gmail.com>
2024-07-28 19:09:57 +0330

    code cleanup and added some comments

--- brands/views.py
+++ brands/views.py
@@ -5,16 +5,20 @@ from .models import Brand
 from django.http import Http404
 
 def createBrand(request):
+    # if request is a get request
     if request.method == "GET":
+        # a empty instance of form
         brand_form = CreateBrandForm()
     else:
         brand_form = CreateBrandForm(request.POST)
-
+        # check if form data is valid
         if brand_form.is_valid():
             brand_form.save()
+            # showing a message to user that the record created
             messages.success(request , "new brand added successfully")
+            return redirect("new-brand")
+    
         
-        return redirect("new-brand")
 
     context = {
         "form": brand_form
@@ -23,21 +27,21 @@ def createBrand(request):
 
 
 def getBrands(request):
+    # getting query parameters
     brandQueryParam = request.GET.get("brand")
     regionQueryParam = request.GET.get("region")
-
-
-    print(brandQueryParam, regionQueryParam)
     
-    
+    # getting all of brands and filtering them based on query parameters
     brands = Brand.objects.all()
 
     if brandQueryParam is not None:
+        # filter them based on brand name without considering if it is uppercase or not 
         brands = brands.filter(name__iexact=brandQueryParam)
     if regionQueryParam is not None:
+        # filtering base on region
         brands = brands.filter(region__icontains=regionQueryParam)
 
-    
+    # if filtering result comes empty
     if brands.count() < 1:
         raise Http404 
 
