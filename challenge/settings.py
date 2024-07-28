f171e122b9dcb4da26701c477bfb6d2966ca253c
unknown <rouchashoori@gmail.com>
2024-07-28 19:25:50 +0330

    set debug to false

--- challenge/settings.py
+++ challenge/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4#y3(2hdwmdo3sllsp(8nng37$$8yr75e+v1%j7%45=h(*=+ie'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
